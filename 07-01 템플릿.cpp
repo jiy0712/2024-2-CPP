a6f5ec8a3f0b944a0616e70879d1d1b30b21b044
jiy11 <jiy112207@gmail.com>
2024-08-27 16:04:19 +0900

    07-01 템플릿 변경 후

--- 07-01 템플릿.cpp
+++ 07-01 템플릿.cpp
@@ -14,8 +14,12 @@ string sum(string a, string b) {
 	return a + b;
 }
 
+template <typename T> T sum(T a, T b) {
+	return a + b;
+}
+
 void main(void) {
-	cout << sum(1, 2) << endl;
-	cout << sum(1.f, 2.f) << endl;
-	cout << sum("1", "2") << endl;
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
 }
