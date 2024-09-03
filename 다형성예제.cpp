c18dd541660a82a578cca4b08d6b61fe613259b1
jiy11 <jiy112207@gmail.com>
2024-09-03 13:57:18 +0900

    다형성 적용 전

--- 다형성예제.cpp
+++ 다형성예제.cpp
@@ -8,6 +8,14 @@ class Food {
 public:
 	Food(int civil, int force, string name, int territory)
 		: civil_(civil), force_(force), name_(name), territory_(territory) {}
+
+	void show() {
+		cout << "±¹¹Î ¼ö : " << civil_ << endl;
+		cout << "±º·Â : " << force_ << endl;
+		cout << "ÀÌ¸§ : " << name_ << endl;
+		cout << "¸éÀû : " << territory_ << endl;
+	}
+
 private:
 	int civil_; //±¹¹Î¼ö
 	int force_; //±º·Â
@@ -19,6 +27,11 @@ class Kimchi : public Food {
 public:
 	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper)
 		:Food(civil, force, name, territory), garlic_(garic), pepper_(pepper) {}
+
+	void show() {
+		cout << "¸¶´Ã : " << garlic_ << endl;
+		cout << "°íÃß : " << pepper_ << endl;
+	}
 private:
 	int garic_;
 	int pepper_;
@@ -28,6 +41,10 @@ class Jelly : public Food {
 public:
 	Jelly(int civil, int force, string name, int territory, int gelatin, int suger)
 		:Food(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar) {}
+	void show() {
+		cout << "Á©¶óÆ¾ : " << gelatin_ << endl;
+		cout << "¼³ÅÁ : " << suger_ << endl;
+	}
 private:
 	int gelatin_;
 	int suger_; //¼³ÅÁÀÇ Èû
@@ -35,10 +52,25 @@ private:
 
 class Cheese : public Food {
 public:
-	Cheese(int civil, int force, string name, int territory, int milk, int Rennet)
-		:Food(civil, force, name, territory), milk_(milk), Rennet_(Rennet) {}
+	Cheese(int civil, int force, string name, int territory, int milk, int rennet)
+		:Food(civil, force, name, territory), milk_(milk), rennet_(rennet) {}
+	void show() {
+		cout << "¿ìÀ¯ : " << milk_ << endl;
+		cout << "ÀÀ°íÁ¦ : " << rennet_ << endl;
+	}
 private:
 	int milk_;
-	int Rennet_; //ÀÀ°íÁ¦
+	int rennet_; //ÀÀ°íÁ¦
 
 };
+
+void main(void) {
+	Food* player = new Kimchi(15, 95, "°«±èÄ¡", 86, 100, 100);
+	Food* friends = new Cheese(100, 20, "Â¥°èÄ¡", 100, 20, 100);
+
+	friends->show();
+	player->show();
+
+	delete friends;
+	delete player;
+}
