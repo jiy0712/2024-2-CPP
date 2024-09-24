e4d1f39acd5daf081fbcbd4b4206a2db04ba6369
jiy11 <jiy112207@gmail.com>
2024-09-24 15:48:26 +0900

    08-02 맵

---
+++ 08-02 맵.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void main(void) {
+
+	map<string, long long> money;
+
+	//¸Ê Ãß°¡
+	money["Áö¼ö"] = 100000000;
+	money["¼öºó"] = 300000000;
+	money["¹Î¼±"] = 100;
+
+	//¹Ýº¹ÀÚ
+	map<string, long long>::iterator iter;
+	for (iter = money.begin(); iter != money.end(); iter++)
+		cout << iter->first <<"´Â" << iter->second << endl;
+
+	//°ª ¼öÁ¤
+	money["¹Î¼±"] = 100000000;
+	cout << money["¹Î¼±"] << endl;
+
+	//Å©±â
+	cout << money.size() << endl;
+}//main
